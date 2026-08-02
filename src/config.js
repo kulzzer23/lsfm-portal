ff1240196e0f883e36ab6a7ed17ad20bb7d062d0
kulzzer23 <vadimobormot@gmail.com>
2026-08-02 16:44:44 +0300

    Vpn

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Supabase Configuration
-export const supabaseUrl = 'https://carjhvadlstlqtadepcb.supabase.co';
+export const supabaseUrl = 'https://lucky-surf-440b.vadimobormot.workers.dev/';
 export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhcmpodmFkbHN0bHF0YWRlcGNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3Mzc2NDcsImV4cCI6MjEwMDMxMzY0N30.8lfIq__TesLs72KktYnSRiBInrfXVH1a9ExAXQltNbQ';
 
 // Tables
